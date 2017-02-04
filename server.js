e09d0429b77222dc779db15d4a4a313b3ea9cca8
Jake Tegtmeier <jaketegtmeier@gmail.com>
2017-02-04 20:21:48 +0000

    removed webpack, bc too slow and hmr not working for angular

--- server.js
+++ server.js
@@ -1,88 +1,74 @@
 'use strict';
 
-import path from 'path';
-import express from 'express';
-import mongoose from 'mongoose';
-import webpack from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from './webpack.config.js';
+let express = require('express');
+var mongoose = require('mongoose');
 
-const isDeveloping = process.env.NODE_ENV !== 'production';
-const IPaddress = isDeveloping ? process.env.IP : process.env.IP;
-const port = isDeveloping ? 8080 : process.env.PORT;
-const app = express();
-const mlabDB = mongoose;
+let app = express();
+mongoose.connect('mongodb://mean-app:supercool@ds117909.mlab.com:17909/mean-app-database');
 
-//setup mongoDB database object and connect to mlab
-mlabDB.connect('mongodb://mean-app:supercool@ds117909.mlab.com:17909/mean-app-database');
-let linkSchema = new mlabDB.Schema({
-    id: Number,
-    title: String,
-    category: String,
-    url: String
-}, {
-    collection: 'Links'
-})
-let Link = mlabDB.model('Link', linkSchema);
-let db = mlabDB.connection;
+let linkSchema = new mongoose.Schema(
+    {id: Number, title: String, category: String, url: String},
+    {collection: 'Links'}
+)
+
+let Link = mongoose.model('Link', linkSchema);
+
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Database connected!");
 });
 
-//setup webpack middleware and express server
-//or run production server
-if (isDeveloping) {
-    const compiler = webpack(webpackConfig);
-    const middleware = webpackMiddleware(compiler, {
-        publicPath: webpackConfig.output.publicPath,
-        //contentBase: 'src',
-        stats: {
-            colors: true,
-            hash: false,
-            timings: true,
-            chunks: false,
-            chunkModules: false,
-            modules: false
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html', function(err){
+        if(err){
+            console.log(err);
+            res.status(err.status).end();
         }
-    });
+        else{
+            console.log('Sent:', 'index.html');
+        }
+  });
+});
 
-    app.use(middleware);
-    app.use(webpackHotMiddleware(compiler));
-    app.get('/', (req, res) => {
-        res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'dist/index.html')));
-        res.end();
-    });
-}
-else {
-    app.use(express.static(__dirname + '/dist'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/dist/index.html'));
-    });
-}
+app.get('/public/:name', function(req, res){
+    res.sendFile(__dirname + '/public/'+ req.params.name, function(err){
+        if(err){
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else{
+            console.log('Sent:', req.params.name);
+        }
+  });
+});
 
-//routes for database queries
-app.get('/linkdata/', (req, res) => {
-    if (req.query.q == "links") {
-        Link.find(function(err, links) {
-            if (err) return console.error(err);
+app.get('/public/:filetype/:name', function(req, res){
+    res.sendFile(__dirname + '/public/' + req.params.filetype + '/' + req.params.name, function(err){
+        if(err){
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else{
+            console.log('Sent:', req.params.name);
+        }
+  });
+});
+
+app.get('/data/:name', function(req, res){
+    if(req.params.name == "links"){
+        Link.find(function(err, links){
+            if(err) return console.error(err);
             res.json(links);
         });
     }
-
-    if (req.query.q == "categories") {
-        Link.find().distinct("category", function(err, categories) {
-            if (err) return console.error(err);
+    
+    if(req.params.name == "categories"){
+        Link.find().distinct("category", function(err, categories){
+            if(err) return console.error(err);
             res.json(categories);
         });
     }
 });
 
-//start express
-app.listen(port, IPaddress, function onStart(err) {
-    if (err) {
-        console.log(err);
-    }
-    console.info('Listening on port %s. Open up http://%s:%s/ in your browser.', port, IPaddress, port);
-});
+app.listen(8080);
